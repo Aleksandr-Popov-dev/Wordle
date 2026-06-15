a04f2a8d19092b3f16e39f51c93f896e2a498cf7
Aleksandr Popov <westunpax@gmail.com>
2026-06-15 16:17:59 +0300

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Popov Alexsandr on 15.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
